177a91fd7279dc8b7782238f66fb1ce5334ea5ca
sammanbasnet <sammanbasnet34@gmail.com>
2025-08-01 06:24:13 +0545

    Fix PaymentRoute import path and remove validation middleware

--- trek_web_backend/routes/PaymentRoute.js
+++ trek_web_backend/routes/PaymentRoute.js
@@ -1,11 +1,9 @@
 const express = require("express")
 const router = express.Router();
-const { findAll, save, findById, deleteById, update } = require("../controller/PaymentController");
-const PackageValidation = require("../validation/PaymentValidation");
-
+const { findAll, save, findById, deleteById, update } = require("../controllers/PaymentController");
 
 router.get("/", findAll);
-router.post("/", PaymentValidation, save);
+router.post("/", save);
 router.get("/:id", findById);
 router.delete("/:id", deleteById);
 router.put("/:id", update)
